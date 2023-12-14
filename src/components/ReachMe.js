4ca57474df80c756e38506baff80bff24dd70b78
Dmitrii <dimaorlov2002@gmail.com>
2023-12-13 22:14:08 -0600

    Added new files

--- src/components/ReachMe.js
+++ src/components/ReachMe.js
@@ -1,3 +1,41 @@
-export default function ReachMe(){
-    
-}
+// ReachMeForm.js
+
+import React, { useState } from 'react';
+import './ReachMe.css';
+
+const ReachMe = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // TODO: Add code to handle form submission
+  };
+
+  return (
+    <div class = 'reach'>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Message:
+        <textarea value={message} onChange={(e) => setMessage(e.target.value)} />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+    </div>
+  );
+};
+
+export default ReachMe;
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,1 +1,16 @@
-export default function Resume() {}
+import resumePDF from './Dmitriis Resume.pdf';
+import './Resume.css';
+
+export default function Resume() {
+    return(
+        <div class='resume'>
+        <h1>My Resume</h1>
+        <iframe
+          title="Resume"
+          src={resumePDF}
+        >
+          This browser does not support PDFs. Please download the PDF to view it.
+        </iframe>
+      </div>
+    )
+}
